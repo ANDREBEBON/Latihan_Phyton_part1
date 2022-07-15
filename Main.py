f48e3ffecc21e8ab41d813bfbf4984f8f72a9130
ANDREBEBON <andrebebon@students.amikom.ac.id>
2022-07-16 01:47:12 +0700

    LatihanPyhton

---
+++ Main.py
@@ -0,0 +1,28 @@
+print(" ")
+
+print("===========================================")
+print("        KALKULATOR        ")
+print("===========================================")
+a = int(input ("Masukan Angka Ke-1\t:"))
+b = int (input ("Masukan Angka Ke-2\t:"))
+c = int (input ("Masukan Angka Ke-3\t:"))
+d = int (input ("Masukan Angka Ke-4\t:"))
+print("===========================================")
+kali = a * b * c * d
+print("Hasil perkalian\t\t:",kali)
+jumlah = a + b + c + d
+print("Hasil penjumlahan\t:",jumlah)
+kurang = a - b - c - d
+print("Hasil pengurangan\t:",kurang)
+bagi = float (a / b / c / d)
+print("Hasil pembagian\t\t:",bagi)
+sisa = float (a % b % c % d)
+print("Hasil Modulus\t\t:",sisa)
+bulat = float (a // b // c // d)
+print("Hasil BagiBulat\t\t:",bulat)
+print("===========================================")
+print(" ")
+
+
+
+
